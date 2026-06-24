cab280f7e85c5f24f8ad9fab282b4ff9340d242f
Anzor1234 <168646401+Anzor1234@users.noreply.github.com>
2026-06-24 19:45:03 +0500

    Update script.js

--- script.js
+++ script.js
@@ -231,7 +231,7 @@ function openProduct(id){
         <div class="pd-call-box-title">To order or ask for the price:</div>
         <div class="pd-call-num">
           <svg viewBox="0 0 24 24" fill="none" stroke-width="1.5"><path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07A19.5 19.5 0 0 1 4.69 12 19.79 19.79 0 0 1 1.62 3.38 2 2 0 0 1 3.6 1.21h3a2 2 0 0 1 2 1.72c.127.96.361 1.903.7 2.81a2 2 0 0 1-.45 2.11L7.91 8.85a16 16 0 0 0 6 6l.95-.95a2 2 0 0 1 2.11-.45c.907.339 1.85.573 2.81.7A2 2 0 0 1 22 16.92z"/></svg>
-          +998 91 539-39-0
+          +998 91 530 93 90
         </div>
         <div class="pd-call-hint">Mon–Sat, 9am–7pm · We'll give you today's price immediately</div>
       </div>
