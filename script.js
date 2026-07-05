f588ab99faeed18640fc57a815d3b4ebfe8a3761
Anzor1234 <168646401+Anzor1234@users.noreply.github.com>
2026-07-05 21:27:15 +0500

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ async function loadProducts() {
     initHome();
     renderCatalog();
 }
+loadProducts();
 const COUNTRIES = [
   {flag:'🇺🇸',name:'United States',code:'+1'},{flag:'🇬🇧',name:'United Kingdom',code:'+44'},
   {flag:'🇩🇪',name:'Germany',code:'+49'},{flag:'🇫🇷',name:'France',code:'+33'},
